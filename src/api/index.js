3b508c361d23da7641e6f9c977fee2fcff2fed36
mori <genixxavier@gmail.com>
2018-07-26 23:19:10 -0500

    dfsfsfsf

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config';
 
 const {apikey} = config;
-const URL = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=b5df30955909e1fb7181c8cbc6a64c39&format=json';
+const URL = 'https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=b5df30955909e1fb7181c8cbc6a64c39&format=json';
 
 export default function  getartistas(country){
     const url = URL.replace(':country',country)
